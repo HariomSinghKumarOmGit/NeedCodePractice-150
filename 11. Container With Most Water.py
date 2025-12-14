616b49fe5252433531b10c4d6d8c828e31180dc4
Hariom Singh <suppraaaa2003@gmail.com>
2025-12-14 08:05:07 +0530

    area will be max

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -6,6 +6,6 @@ class Solution:
         for l in range(len(height)):
             for r in range(l+1, len(height)):
                 area = (r-l)*min(height[l], height[r])
-                res = min (res, area)
+                res = max (res, area)
 
         return res
